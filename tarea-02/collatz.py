419b15376269f46660172bc205c024d4694c63a3
nestor2502 <nestor2502@ciencias.unam.mx>
2022-08-29 20:08:28 -0500

    se termina tarea 02

--- tarea-02/collatz.py
+++ tarea-02/collatz.py
@@ -2,6 +2,10 @@ print("input a number: ")
 number = input()
 number = int(number)
 
+hs = open("attemps.txt","a")
+hs.write(str(number) + '\n')
+hs.close() 
+
 def collatz(number):
     if number %2 == 0:
         return number//2
